52f7f54e66ea750c3669591f92500616a1c0d138
Jonathan <j.giardino@me.com>
2022-10-01 08:34:46 +0200

    Fix header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Dropdown from "./Dropdown";
 
 const Header: FC = () => {
   return (
-    <nav className="w-full py-3 px-4 flex justify-between items-center fixed z-50 backdrop-blur-md">
+    <nav className="w-full py-3 px-4 flex justify-between items-center md:fixed z-50">
       <div className="flex gap-1 items-center">
         <span className="font-bold text-xl tracking-tighter text-gray-400 hover:text-gray-600 transition cursor-pointer duration-300 ease-out">
           {new Date().toDateString()}

8db3d089268cf3ebdc359d6ac5c678eecafc821e
Jonathan <j.giardino@me.com>
2022-10-01 08:18:19 +0200

    Reverse last commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      <main className="flex w-full max-w-[768px] flex-1 flex-col-reverse">
+      <main className="flex w-full max-w-[768px] flex-1 flex-col-reverse mb-10">
         <TipTap />
       </main>
     </div>

79b7fb1985f5e4b1f45360326e6c7684e6af174f
Jonathan <j.giardino@me.com>
2022-10-03 21:38:16 +0200

    remove custom document

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,24 @@ const Home: NextPage = () => {
     <div className="flex min-h-screen flex-col items-center justify-start">
       <Head>
         <title>minutes</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <Header selectedDate={selectedDate} setSelectedDate={setSelectedDate} />

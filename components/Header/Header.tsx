47f187b4eb9a168478f859ab80ba686532ca9219
Jonathan <j.giardino@me.com>
2022-10-03 22:34:21 +0200

    small fixes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -33,7 +33,7 @@ const Header: FC<{ selectedDate: string; setSelectedDate: any }> = ({
         setSelectedDate={setSelectedDate}
       />
       <div className="flex gap-3 items-center">
-        <Icon type="logo" className="w-7" />
+        <Icon type="logo" className="w-8 md:w-5" />
         <button
           aria-label="Date button"
           className="hidden md:flex gap-4 items-center focus:outline-brand"
@@ -52,7 +52,7 @@ const Header: FC<{ selectedDate: string; setSelectedDate: any }> = ({
         className="flex md:hidden gap-4 items-center focus:outline-brand bg-gray-200 dark:bg-[#333338] md:bg-transparent px-3 py-1 rounded-lg md:py-o md:px-0"
         onClick={() => setOpen(!open)}
       >
-        <span className="font-bold text-xl tracking-tighter text-gray-400 hover:text-gray-600 dark:text-[#7d8082] dark:hover:text-[#a3a7aa] transition cursor-pointer duration-300 ease-out">
+        <span className="font-bold text-lg md:text-xl tracking-tighter text-gray-400 hover:text-gray-600 dark:text-[#7d8082] dark:hover:text-[#a3a7aa] transition cursor-pointer duration-300 ease-out">
           {selectedDate}
         </span>
         <HiOutlineCalendar className="text-gray-400 hover:text-gray-600 dark:text-[#7d8082] dark:hover:text-[#a3a7aa]" />

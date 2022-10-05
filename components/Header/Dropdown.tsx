2b19b6cb2d4e76749cad8eb6b32794caa62eddb4
Jonathan <j.giardino@me.com>
2022-10-05 16:12:29 +0200

    Add login dropdown UI

--- components/Header/Dropdown.tsx
+++ components/Header/Dropdown.tsx
@@ -1,8 +1,10 @@
 import React, { FC, Fragment } from "react";
 import clsx from "clsx";
 import { Menu, Transition } from "@headlessui/react";
+import { FiLogIn, FiLogOut } from "react-icons/fi";
 
 const Dropdown: FC = () => {
+  const logged = true;
   return (
     <Menu as="div" className="relative flex">
       <Menu.Button className="h-[39px] w-[39px] rounded-full bg-gradient-to-r from-red-400 to-brand cursor-pointer hover:shadow-2xl hover:opacity-90 transition ease-out duration-300" />
@@ -16,82 +18,138 @@ const Dropdown: FC = () => {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="absolute right-0 z-50 w-64 mt-12 origin-top-right bg-gray-200 dark:bg-[#333338] dark:text-[#f8fafc] rounded-lg shadow-sm ring-1 ring-black ring-opacity-5 focus:outline-brand">
-          <div className="px-4 py-3">
-            <p className="text-base ">Signed in as</p>
-            <p className="truncate text-base  font-semibold">
-              hello@jonathangiardino.com
-            </p>
-          </div>
-          <div className="py-2">
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={clsx(
-                    active
-                      ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
-                      : "",
-                    "block px-4 py-2 text-base rounded-md mx-1"
+          {logged ? (
+            <>
+              <div className="px-4 py-3">
+                <p className="text-base ">Signed in as</p>
+                <p className="truncate text-base  font-semibold">
+                  hello@jonathangiardino.com
+                </p>
+              </div>
+              <div className="py-2">
+                {/* <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={clsx(
+                        active
+                          ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
+                          : "",
+                        "block px-4 py-2 text-base rounded-md mx-1"
+                      )}
+                    >
+                      Account settings
+                    </a>
                   )}
-                >
-                  Account settings
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={clsx(
-                    active
-                      ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
-                      : "",
-                    "block px-4 py-2 text-base  rounded-md mx-1"
+                </Menu.Item> */}
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={clsx(
+                        active
+                          ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
+                          : "",
+                        "block px-4 py-2 text-base  rounded-md mx-1"
+                      )}
+                    >
+                      Support
+                    </a>
                   )}
-                >
-                  Support
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={clsx(
-                    active
-                      ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
-                      : "",
-                    "block px-4 py-2 text-base  rounded-md mx-1"
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={clsx(
+                        active
+                          ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
+                          : "",
+                        "block px-4 py-2 text-base  rounded-md mx-1"
+                      )}
+                    >
+                      Feedback
+                    </a>
                   )}
-                >
-                  Feedback
-                </a>
-              )}
-            </Menu.Item>
-          </div>
-          <div className="py-2 px-1">
-            <form method="POST" action="#">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type="submit"
-                    className={clsx(
-                      active
-                        ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
-                        : "",
-                      "block w-full px-4 py-2 text-left text-base rounded-md font-bold"
+                </Menu.Item>
+              </div>
+              <div className="py-2 px-1">
+                <form method="POST" action="#">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        type="submit"
+                        className={clsx(
+                          active
+                            ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
+                            : "",
+                          "flex items-center gap-2 w-full px-4 py-2 text-left text-base rounded-md font-bold"
+                        )}
+                      >
+                        <span>Sign out</span>
+                        <FiLogOut />
+                      </button>
                     )}
-                  >
-                    Sign out
-                  </button>
-                )}
-              </Menu.Item>
-            </form>
-          </div>
+                  </Menu.Item>
+                </form>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={clsx(
+                        active
+                          ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
+                          : "",
+                        "flex items-center gap-2 px-4 py-2 text-base rounded-md mx-1 font-bold"
+                      )}
+                    >
+                      <span>Sign in</span>
+                      <FiLogIn />
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={clsx(
+                        active
+                          ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
+                          : "",
+                        "block px-4 py-2 text-base  rounded-md mx-1"
+                      )}
+                    >
+                      Support
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={clsx(
+                        active
+                          ? "bg-gray-300 text-gray-900 dark:bg-[#45454d] dark:text-white"
+                          : "",
+                        "block px-4 py-2 text-base  rounded-md mx-1"
+                      )}
+                    >
+                      Feedback
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            </>
+          )}
         </Menu.Items>
       </Transition>
     </Menu>
   );
 };
 
-export default Dropdown
+export default Dropdown;
